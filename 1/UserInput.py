c9fd3ed60b891bfeee44aabbf2348895e3920afb
Connor Hackenberg <connorhackenberg@gmail.com>
2023-08-23 17:41:35 -0400

    Create UserInput.py
    Add second v1 file

---
+++ 1/UserInput.py
@@ -0,0 +1,13 @@
+# Made by Connor Hackenberg
+# Date Started: 8/23/2023
+# Program Input: This program will accept user input.
+# Contact: https://connorhackenberg.tech/
+# Mirrored on GitHub github.com/MapGuy11
+
+
+# Hardcoded Version of Program
+hoursWorked = 18
+payRate = 13.13
+grossPay = hoursWorked * payRate
+print ("Your gross pay is: $" + str(grossPay))
+quit()
