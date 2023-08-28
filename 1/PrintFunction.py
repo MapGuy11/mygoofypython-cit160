2704f9fa9e49094c3faf0e4df84e9c0ec02e76d5
Connor Hackenberg <connorhackenberg@gmail.com>
2023-08-28 17:42:21 -0400

    8-28-23

---
+++ 1/PrintFunction.py
@@ -0,0 +1,5 @@
+# Made by Connor Hackenberg
+# Date Started: 8/28/2023
+# Program Description: This program will demonstrate will explore the print function in more depth.
+# Contact: https://connorhackenberg.tech/
+# Mirrored on GitHub github.com/MapGuy11
